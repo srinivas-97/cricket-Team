92d2a14617cfb2fd3a8fa314b8849875a7e8578c
srinivas <srinu781997@gmail.com>
2023-02-12 06:03:08 +0000

    some code add

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+const app = express();
+app.use(express.json());
+const dbPath = path.join(__dirname, "cricketTeam.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+initializeDBAndServer();
+
+// Get players API
+app.get("/players/", async (request, response) => {
+  const getPlayersQuery = `
+    SELECT
+      *
+    FROM
+      player
+    ORDER BY
+      player_id;`;
+  const playersArray = await db.all(getPlayersQuery);
+  response.send(playersArray);
+});
